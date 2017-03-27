80097e2a67bdf02951ef4c8b0e7f34c68722ec90
EmilyH <EmilyHHLam@gmail.com>
2017-03-26 23:56:32 -0500

    add the complete function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,40 +1,32 @@
-$(document).ready(function(){
-console.log('jQuery sourced');
-
-var itteam = {};
 var current=0;
 var taskId ;
 var ticketDetail;
 var employeeId;
-
-
+var employees = [];
+$(document).ready(function(){
+console.log('jQuery sourced');
 //showing the task list
 getTask();
-
 //showing the work-on-progress
 workOnProgress();
-
-$(".taskinfo #target").change( function() {
-  console.log('come here');
-// var selectedOption = $("#targetEmployee option:selected");
-// var selectedValue = selectedOption.val();  // gets the selected value
-//  var selectedText = selectedOption.text();  // gets the selected text
-});
-
 
 $('.taskinfo').on('click', '.assignnow', function() {
   console.log('here')
   console.log($(this).data("employeeid"));
   console.log($(this).data("id"));
   console.log($(this).data("detail"));
-  $('.taskinfo').empty();
+  var taskId = $(this).data("id");
+  var selectEL = $('#target' + taskId);
+  console.log('selectId' + selectEL.val());
+  var employeeId = selectEL.val();
   $.ajax( {
   type: 'POST',
   url: 'task/assigntask/',
-  data: {task_id: $(this).data("id"), employee_id: $(this).data("employeeid") },
-  sucess: function(response){
-    console.log(response);
+  data: {task_id: taskId, employee_id: employeeId },
+  success: function(response){
+    console.log('assign' + response);
       getTask();
+      workOnProgress();
   }
 });//end of ajax
 });//end of clicking taskinfo
@@ -52,6 +44,17 @@ $('.working').on('click', '.complete', function() {
       }//end success function
     });//end of ajax
 });//end of working div
+
+//complete then delete
+$('.working').on('click', '.delete', function() {
+  //console.log('delete here' + $(this).data('id'));
+  var checkstr =  confirm('are you sure you want to delete task ' + $(this).data('id') + '?');
+  if(checkstr == true){
+    console.log('delete here' + $(this).data('id'));
+  }else{
+  return false;
+  }
+  })
 
 $('.addingTask').on('click', '#ticketSubmited', function() {
   console.log('ticketid ' + $('#ticketid').val());
@@ -71,63 +74,73 @@ $('.addingTask').on('click', '#ticketSubmited', function() {
 });//end document.ready
 
 function getTask() {
+  getEmployees();
   $.ajax({
     type: "GET",
     url: "/task",
     success: function(response) {
-      console.log(response);
-      $('.taskinfo').empty();
-      for(var i = 0; i < response.length; i++) {
-        var task = response[i];
-        taskId = task.id;
-        ticketDetail = task.detail;
-        $('.taskinfo').append('<tr></tr>');
-        var $el = $('.taskinfo').children().last();
-        $el.append('<td>' + taskId+ '</td>');
-        $el.append('<td>' + task.ticket_id + '</td>');
-        $el.append('<td>' + ticketDetail + '</td>');
-        getEmployee();
-      }//end for loop
+      console.log('Tasks: ' + response);
+      displayTaskList(response);
+
     }//end sucess
   });//end ajax
 }//end the gettask()
 //  NOT WORKING - try to grab the assiging employee
+function displayTaskList(tasks) {
+  $('.taskinfo').empty();
+  for(var i = 0; i < tasks.length; i++) {
+    var task = tasks[i];
+    taskId = task.id;
+    ticketDetail = task.detail;
+    $('.taskinfo').append('<tr></tr>');
+    var $el = $('.taskinfo').children().last();
+    $el.append('<td>' + taskId+ '</td>');
+    $el.append('<td>' + task.ticket_id + '</td>');
+    $el.append('<td>' + ticketDetail + '</td>');
+    var option = displayEmployeeList(employees);
+    $el.append('<td><select id ="target' + taskId + '">' + option + '</select></td>');
+    $el.append('<td><button class="assignnow" data-id="' +
+                taskId +'" data-detail= "' + ticketDetail + '" data-employeeid= "' + employeeID + '">assign task</button>');
 
-//dropdown list of empployees
-var employee =[];
-function getEmployee() {
 
-var option = '';
-var numbers;
+  }//end for loop
+  $(".taskinfo #target").change( function() {
+    console.log('come here');
+  // var selectedOption = $("#targetEmployee option:selected");
+   var selectedValue = selectedOption.val();  // gets the selected value
+   console.log(selectValue);
+  //  var selectedText = selectedOption.text();  // gets the selected text
+  });
+
+}
+//dropdown list of employees
+
+function getEmployees() {
+  var numbers;
   $.ajax({
-  type: "GET",
-  url:'/task/employee',
-  success: function(employee) {
-    console.log(employee);
+    type: "GET",
+    url:'/task/employee',
+    success: function(response) {
+      console.log(employees);
+        employees = response;
 
   }//end loop
   });
   //NOT WORKING - how to get the array out of the function
-console.log('outside=' + employee);
-  var employee = [{"id":1,"first_name":"Emily"},
-                  {"id":2,"first_name":"Brit"}];
-
-    for (var i=0;i<employee.length;i++){
-      employeeID = employee[i].id ;
-
-      //var employee = response[i];
-       option += '<option value="'+ employeeID + '">' + employee[i].first_name + '</option>';
-    }
-
-  var $el = $('.taskinfo').children().last();
-  $el.append('<td><select id ="target">' + option + '</select></td>');
-  $el.append('<div id="result1"></div>');
-  $el.append('<td><button class="assignnow" data-id="' +
-                      taskId +'" data-detail= "' + ticketDetail + '" data-employeeid= "' + employeeID + '">assign task</button>');
 
 
+}
 
+function displayEmployeeList(employees) {
+        var option = '';
 
+        for (var i=0;i<employees.length;i++){
+            employeeID = employees[i].id ;
+
+             option += '<option value="'+ employeeID + '">' + employees[i].first_name + '</option>';
+        }
+
+        return option;
 }
 //work-on-progress
 function workOnProgress() {
@@ -145,19 +158,22 @@ function workOnProgress() {
         var employeeName = task.first_name;
         var complete =task.complete;
         console.log(complete);
-
-        $('.working').append('<tr></tr>');
+        if (complete) {
+          $('.working').append('<tr bgcolor="#FF0000"></tr>');
+        }else {
+          $('.working').append('<tr></tr>');
+        }
         var $el = $('.working').children().last();
         $el.append('<td>' + taskId+ '</td>');
         $el.append('<td>' + ticketId + '</td>');
         $el.append('<td>' + detail + '</td>');
         $el.append('<td>' + employeeName + '</td>');
         if (complete) {
-        $el.append('<td>yes</td>')
+        $el.append('<td align="middle"><img width="15px" height="15px" src="https://balance.ua/img/news/5P8rB9KTzPEbTezNS64JxF50f0PuOCNi_preview/14788457491008.jpg"></td>')
         }else {
         $el.append('<td><button class="complete" data-id="'+
                         taskId +'">Complete</button>');
-                      }
+        }
         $el.append('<td><button class="delete" data-id="'+
                         taskId +'">Delete</button>');
       }//end for loop
--- task.sql
+++ task.sql
@@ -13,10 +13,12 @@ CREATE TABLE taskinfo (
 CREATE TABLE tasking(
     id SERIAL PRIMARY KEY,
     task_id integer REFERENCES taskinfo,
-    employee_id  integer REFERENCES itteam,
-    complete BOOLEAN NOT NULL,
-    delete BOOLEAN NOT NULL
+    employee_id  integer REFERENCES employee,
+    complete BOOL NOT NULL DEFAULT '0'
+   
 );
+
+
 
 
 INSERT INTO employee VALUES 
@@ -28,3 +30,25 @@ INSERT INTO employee VALUES
 (6,'Craig', 'Baird', 'QA'),
 (7,'Lisa', 'Schoofs', 'QA')
 ;
+
+SELECT * FROM "warehouse" JOIN "warehouse_product"
+ON "warehouse"."id" = "warehouse_product"."product_id"
+JOIN "products" ON "products"."id" = "warehouse_product"."product_id"
+WHERE "products"."description" = 'diet pepsi';
+
+SELECT "taskinfo"."id" as id, "taskinfo"."ticket_id", "taskinfo"."detail" FROM "taskinfo" 
+LEFT JOIN "tasking"
+ON "taskinfo"."id" = "tasking"."task_id"
+WHERE "tasking"."task_id" IS NULL
+ORDER BY "taskinfo"."id" DESC;
+
+--SELECT "tasking"."task_id" as "task_id", "taskinfo"."detail" , "taskinfo"."ticket_id", "employee"."first_name" as first_name FROM "taskinfo"
+SELECT "tasking".*, "taskinfo".*  FROM "taskinfo"
+JOIN "tasking" ON "tasking"."task_id" = "taskinfo"."id"
+JOIN "employee" ON "employee"."id" = "tasking"."employee_id"
+ORDER BY "tasking"."task_id";
+
+WHERE "tasking"."id" = '11';
+WHERE "tasking"."complete" IS FALSE
+
+UPDATE "tasking" 
